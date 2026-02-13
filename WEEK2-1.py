62e5c97e476bce5bc49e16db5eabcd3c8008ec2a
24331a05e3-cell <24331a05e3@mvgrce.edu.in>
2026-02-13 11:56:10 +0530

    Add files via upload

---
+++ WEEK2-1.py
@@ -0,0 +1,7 @@
+#24331A05E3
+#Simple Intrest
+p=float(input("Enter principle amount: "))
+r=float(input("Enter rate of intrest: "))
+t=float(input("Enter time period(in yr): "))
+SI=(p*r*t)/100
+print("Simple Intrest is ",SI)
